6f5e3f347990e136d3265d7327cff6f3ad58e43d
AndroidWG <samu-rodrigues@hotmail.com>
2020-05-24 19:56:05 -0300

    Added romajiSystem param and  commenting, logging

--- app.js
+++ app.js
@@ -6,38 +6,50 @@ var app = express(); app.listen(process.env.PORT || 3000, () => {
 const Kuroshiro = require('kuroshiro');
 const KuroshiroAnalyzer = require('kuroshiro-analyzer-kuromoji');
 
-app.get("/romaji", async (request, response, next) => {
+//the only part that matters
+//Parameters:
+//[text]-text to convert
+//[to]-convert to one of these: hiragana, katakana, romaji
+//[mode]-using one of these modes: normal, spaced, okurigana, furigana
+//[romajiSystem]-using one of these systems: nippon, passport, hepburn
+app.get("/convert", async (request, response, next) => {
     try {
+        console.log("Recived convert request from ${request.ip}");
+        
         const kuroshiro = new Kuroshiro();
         await kuroshiro.init(new KuroshiroAnalyzer({ dictPath: 'node_modules/kuromoji/dict' }));
 
+        console.log("Parameters: ${request.params}");
         const text = request.query.text;
         const to = request.query.to;
         const mode = request.query.mode;
+        //will use hepburn if param doesn't exist
+        const romajiSystem = (request.query.mode == null) ? "hepburn" : request.query.mode;
 
-        console.log(text);
+        console.log("Original text: ${text}");
 
+        //split to convert line by line
         const lines = text.split("\n")
-        console.log(lines);
 
-        const romajilines = await Promise.all(lines
+        const convertedlines = await Promise.all(lines
             .map(async line => {
                 if (Kuroshiro.Util.hasJapanese(line)){
-                    return await kuroshiro.convert(line, { to: to, mode: mode });
+                    return await kuroshiro.convert(line, { to: to, mode: mode, romajiSystem: romajiSystem});
                 } else {
                     return line;
                 }
             }))
             
-        const romaji = romajilines.join("<br>");
-        console.log(romaji);
-        response.send(romaji);
+        const converted = convertedlines.join("<br>");
+        console.log("Comverted output: ${converted}");
+        response.send(converted);
     }
     catch (e) {
         next(e);
     }
 });
 
+//simple page to show if the main URL is accessed
 app.get('/', function (request, response) {
     response.sendFile(__dirname + '/views/index.html');
 });
